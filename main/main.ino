b7579d569443abd0f06ad1e59aaac3cf6fb8ac27
OGUNLOLU DANIEL <amazing.comptech@gmail.com>
2021-06-13 07:38:44 -0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,36 @@
-String command;
+void nextion(String command)
+{
+Serial.print(command); 
+Serial.write(0xff); //We need to write the 3 ending bits to the Nextion as well
+Serial.write(0xff); //it will tell the Nextion that this is the end of what we want to send.
+Serial.write(0xff);
+}
 
 void setup() 
 {
   pinMode(A0, INPUT);
+  pinMode(14, INPUT);
+  pinMode(12, INPUT);
   Serial.begin(9600);
-
 
 }
 
 void loop() 
 {
+
+
+if((digitalRead(14) == 1)||(digitalRead(12) == 1))
+{
+nextion("detect.txt=\"NO ECG\"");
+}
+else{
+//// send the value of analog input 0:
+nextion("detect.txt=\"ECG DETECTED\"");
 Serial.print("add 1,0,"); // Changing the value of box n1
-Serial.print(analogRead(A0));
+Serial.print(analogRead(A0)/2);
 Serial.write(0xff); //We need to write the 3 ending bits to the Nextion as well
 Serial.write(0xff); //it will tell the Nextion that this is the end of what we want to send.
 Serial.write(0xff);
-
+}
 
 }
